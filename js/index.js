bf84246d7db29a1e27454f6346dc9918601f5ca9
Hesham <Heshamoov90@gmail.com>
2019-11-25 10:30:59 +0400

    Date Range

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function initDate(){
+    let today = new Date().toISOString().substr(0, 10);
+    document.querySelector("#to").value = today;
+}
+
+document.getElementById("search").onclick = function() {search()};
+
+function search() {
+    let date = new Date(document.querySelector("#from").value);
+    day = date.getDate();
+    month = date.getMonth() + 1;
+    year = date.getFullYear();
+    let fromdate = year + '-' + month + '-' + day;
+
+
+    date = new Date(document.querySelector("#to").value);
+    day = date.getDate();
+    month = date.getMonth() + 1;
+    year = date.getFullYear();
+    let todate = year + '-' + month + '-' + day;
+
+    document.getElementById("fromCell").innerHTML = fromdate;
+    document.getElementById("toCell").innerHTML = todate;
+}
