496a306e05cd220ae53423d94aac7ef0b2a26ebb
Hesham <Heshamoov90@gmail.com>
2019-12-15 14:31:53 +0400

    Add Statistics in General Page

--- js/index.js
+++ js/index.js
@@ -59,3 +59,18 @@ function general(params) {
                                          + "&familyid=" + familyid, false);
     FamilyStatementRequest.send();
 }
+
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
--- mysql/general.php
+++ mysql/general.php
@@ -6,6 +6,44 @@ $start_date = $_REQUEST["start_date"];
 $end_date = $_REQUEST["end_date"];
 // echo $start_date;
 
+
+$statistics= "
+SELECT 
+COUNT(DISTINCT guardians.first_name) parents, COUNT(DISTINCT students.last_name) students,
+ROUND(SUM(finance_fees.particular_total),0) balance,
+finance_fee_collections.start_date start_date,
+CURDATE() today
+
+FROM guardians 
+
+INNER JOIN students ON guardians.familyid = students.familyid
+INNER JOIN finance_fees ON students.id = finance_fees.student_id
+INNER JOIN finance_fee_collections ON finance_fees.fee_collection_id = finance_fee_collections.id
+
+WHERE STR_TO_DATE(finance_fee_collections.start_date,'%Y-%m-%d') >= '$start_date'
+";
+
+// echo $statistics;
+$result = $conn->query($statistics);
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "<table class='w3-table-all w3-card w3-centered'>";
+        echo "
+            <thead>
+            <tr>
+                <th>Number of Parents "  . $row['parents']  . "</th>
+                <th>Number of Students " . $row['students'] . "</th>
+                <th>Total Balance &nbsp"      . $row['balance']  . " AED</th>
+            </tr>
+            </thead>
+        ";
+        echo "</table><br>";
+    }    
+}else 
+    echo "No Data Found! Try another search.";
+
+
+
 $general= "
 SELECT 
 guardians.first_name  parent,
