2c15188bf97535913dbf62ae0e06a399868ec2eb
Hesham <Heshamoov90@gmail.com>
2019-12-29 14:25:07 +0400

    Navbar
    Signed-off-by: Hesham <Heshamoov90@gmail.com>

--- dashboard.php
+++ dashboard.php
@@ -65,54 +65,8 @@ checkLoggedIn()
 
 
 </head>
-<body onload="currentDate()">
-
-
-<div class="wrapper d-flex align-items-stretch">
-    <nav id="sidebar" class="active" style="background-color: darkred!important;">
-        <h2><a href="finance.php" class="logo"><img src="assets/indepth-logo.jpg" width="60" class="logoImage"></a></h2>
-        <ul class="list-unstyled components mb-5">
-            <li class="active">
-                <a href="dashboard.php"><span class="fa fa-home bd-lightMagenta-active"></span> Home</a>
-            </li>
-            <li>
-                <a href="#"><span class="fa fa-user"></span> Accounts</a>
-            </li>
-            <li>
-                <a href="#"><span class="fa fa-sticky-note"></span> Ledger</a>
-            </li>
-            <li>
-                <a href="#"><span class="fa fa-cogs"></span> Services</a>
-            </li>
-            <li>
-                <a href="#"><span class="fa fa-paper-plane"></span> Messages</a>
-            </li>
-        </ul>
-
-        <div class="footer">
-            <p>
-                Copyright &copy;<script>document.write(new Date().getFullYear());</script>
-                All rights reserved by <a href="https://indepth.ae" target="_blank"
-                                          style="color: black; font-weight: bold">INDEPTH</a>
-            </p>
-        </div>
-    </nav>
-
-    <!-- Page Content  -->
-    <div id="content" class="p-4 p-md-3">
-
-        <nav class="navbar navbar-expand-lg navbar-light btn-sm btn-outline-light">
-            <div class="container-fluid">
-
-                <button type="button" id="sidebarCollapse" class="btn btn-sm " style="background-color: darkred">
-                    <i class="fa fa-bars" style="color: white;"></i>
-                    <span class="sr-only">Toggle Menu</span>
-                </button>
-                <button class="btn btn-dark d-inline-block d-lg-none ml-auto btn-sm " style="background-color: darkred"
-                        type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fa fa-bars" style="color: white;"></i>
-                </button>
+<body>
+<?php include('navbar.php'); ?>
                 <h4 style="color:black">INDEPTH FINANCE</h4>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="nav navbar-nav ml-auto">
