d7f14da95e3bffac87ad554c1d8cb71435ca6494
Hesham <heshamoov90@gmail.com>
2020-02-23 15:59:04 +0400

    Daybook Navigate
    Signed-off-by: Hesham <heshamoov90@gmail.com>

--- daybook.php
+++ daybook.php
@@ -47,15 +47,42 @@ checkLoggedIn()
 
     <script>
         $(document).ready(function () {
+            // Icon Color
             document.getElementById('daybook').style.color = '#25221E';
+
+            // todays Date
+            let today = new Date().toISOString().substr(0, 10);
+            document.getElementById("todays_date").innerText = today + " - ";
+            // document.getElementById("out").innerHTML = "Today " + new Date().toUTCString();
         });
     </script>
+    <script>
+        function startTime() {
+            var today = new Date();
+            var h = today.getHours();
+            var m = today.getMinutes();
+            var s = today.getSeconds();
+            m = checkTime(m);
+            s = checkTime(s);
+            document.getElementById('todays_time').innerHTML =
+                h + ":" + m + ":" + s;
+            var t = setTimeout(startTime, 500);
+        }
 
+        function checkTime(i) {
+            if (i < 10) {
+                i = "0" + i
+            }
+            ;  // add zero in front of numbers < 10
+            return i;
+        }
+    </script>
 </head>
 
-<body>
+<body onload="startTime()">
+
 <?php include('navbar.php'); ?>
-<h4 style="color:black">Dashboard</h4>
+<h4 style="color:black">Daybook</h4>
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="nav navbar-nav ml-auto">
         <li class="nav-item ">
@@ -75,5 +102,63 @@ checkLoggedIn()
 </div>
 </nav>
 
+<div class="container">
+    <div class="row todays_date">
+        <h4 id="todays_date"></h4>
+        <h4 id="todays_time"></h4>
+    </div>
+</div>
 
+<div class="row todays_transactions">
+    <table class="table table-hover">
+        <thead class="black white-text">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Payment Mode</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Balance</th>
+            <th scope="col">Grade</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+</div>
+
+
+</div> <!--navbar-->
+</div> <!--navbar-->
+
+
+<script>
+    document.getElementById('navFollowUp').style.color = '#25221E';
+    $(document).ready(function () {
+        search();
+    });
+</script>
+
+<script src="js/sms.js"></script>
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/calender.js"></script>
 </body>
+</html>
