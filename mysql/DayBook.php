fce3daf73ea3df48bf2f7b68bc5104d6c1cc549e
Hesham <heshamoov90@gmail.com>
2020-02-24 13:54:42 +0400

    Transactions Table in Datatable
    Signed-off-by: Hesham <heshamoov90@gmail.com>

--- mysql/DayBook.php
+++ mysql/DayBook.php
@@ -5,26 +5,38 @@ include_once '../functions.php';
 include('../config/db.php');
 
 $sql = "
-SELECT finance_transactions.id,
+SELECT finance_fee_categories.name,
+       finance_fee_categories.id,
+
+       students.last_name,
+       finance_transactions.payee_id,
+
+       courses.course_name,
+
+       batches.name section,
+       finance_transactions.batch_id,
+
+       finance_transactions.id,
        finance_transactions.title,
        finance_transactions.amount,
        finance_transactions.created_at,
        finance_transactions.transaction_date,
        finance_transactions.finance_id,
-       finance_transactions.payee_id,
+
        finance_transactions.receipt_no,
        finance_transactions.payment_mode,
-       finance_transactions.payment_note,
-       finance_fee_categories.name,
-       finance_transactions.user_id,
-       users.first_name
+       finance_transactions.payment_note
+
 FROM finance_transactions
-         INNER JOIN finance_fee_categories on finance_transactions.category_id = finance_fee_categories.id
-         INNER JOIN users on finance_transactions.payee_id = users.id
+         INNER JOIN finance_fee_categories ON finance_transactions.category_id = finance_fee_categories.id
+         INNER JOIN students ON finance_transactions.payee_id = students.id
+         INNER JOIN batches ON finance_transactions.batch_id = batches.id
+         INNER JOIN courses ON batches.course_id = courses.id
 WHERE STR_TO_DATE(finance_transactions.created_at, '%Y-%m-%d') = '2019-10-07'
 ORDER BY finance_transactions.created_at DESC;
 ";
 
+//echo $sql;
 
 $result = $conn->query($sql);
 
@@ -36,6 +48,7 @@ if ($result->num_rows > 0) {
     <tr>
         <th>#</th>
         <th>FEE</th>
+        <th>GRADE</th>
         <th>AMOUNT</th>
         <th>PAYMENT</th>
         <th>DESCRIPTION</th>
@@ -48,10 +61,11 @@ if ($result->num_rows > 0) {
             <tr>
                 <td>" . ++$row_number . "</td>
                 <td>" . $row['name'] . "</td>
+                <td>" . $row['course_name'] . " - " . $row['section'] . "</td>
                 <td>" . $row['amount'] . "</td>
                 <td>" . $row['payment_mode'] . "</td>
                 <td>" . $row['title'] . "</td>
-                <td>" . $row['first_name'] . "</td>
+                <td>" . $row['last_name'] . "</td>
             </tr>
         ";
     }
