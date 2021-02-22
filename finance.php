10c5828841893661411519c16055ce464274f108
Hesham <heshamoov90@gmail.com>
2021-02-22 16:04:55 +0400

    Release

--- finance.php
+++ finance.php
@@ -36,31 +36,67 @@ checkLoggedIn()
 
     <!--    data table-->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
-    <script type="text/javascript" charset="utf8"   src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>
-    <script type="text/javascript" charset="utf8"   src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
-    <script type="text/javascript" charset="utf8"   src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script type="text/javascript" charset="utf8"   src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script type="text/javascript" charset="utf8"   src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script type="text/javascript" charset="utf8"   src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
-    <script type="text/javascript" charset="utf8"   src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+    <link rel="stylesheet" type="text/css"
+          href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
+    <script type="text/javascript" charset="utf8"
+            src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" charset="utf8"
+            src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" charset="utf8"
+            src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script type="text/javascript" charset="utf8"
+            src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script type="text/javascript" charset="utf8"
+            src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script type="text/javascript" charset="utf8"
+            src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+    <script type="text/javascript" charset="utf8"
+            src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
 
     <!--    print.js-->
     <script type="text/javascript" charset="utf8" src="js/print.min.js"></script>
     <link rel="stylesheet" href="css/print.min.css">
 
     <!-- Exporting table as excel -->
-<!--    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
+    <!--    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
     <script src="js/table2excel.js"></script>
-
 
 
     <script>
         function parentsDataTable() {
+            let date = new Date(document.querySelector("#start_date").value);
+            day = date.getDate();
+            month = date.getMonth() + 1;
+            year = date.getFullYear();
+            let start_date = year + '-' + month + '-' + day;
+
+            date = new Date(document.querySelector("#end_date").value);
+            day = date.getDate();
+            month = date.getMonth() + 1;
+            year = date.getFullYear();
+            let end_date = year + '-' + month + '-' + day;
+
             $('#ParentsTable').DataTable({
                 dom: 'Bfrtip',
                 buttons: [
-                    'copy', 'csv', 'excel', 'pdf', 'print'
+                    'copy',
+                    {
+                        extend: 'excelHtml5',
+                        title: 'Al Sanawbar School \n Parents Report \n (' + start_date + ' to ' + end_date + ')'
+                    },
+                    {
+                        extend: 'pdfHtml5',
+                        title: 'Al Sanawbar School \n Parents Report \n (' + start_date + ' to ' + end_date + ')',
+
+                    },
+                    { extend: 'csv',
+                        title: 'Al Sanawbar School \n Parents Report \n (' + start_date + ' to ' + end_date + ')'
+                    },
+                    { extend: 'print',
+                        title : '',
+                        messageTop: ' <h4 align="center">Al Sanawbar School </h4> <h6 align="center"> Parents  Report  (' + start_date + '-' + end_date + ') </h6>'
+                    }
+
                 ]
             });
             $('.dataTables_length').addClass('bs-select');
