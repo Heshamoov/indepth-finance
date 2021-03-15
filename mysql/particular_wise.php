697b354762e21e1cca598079e7d09723a7459827
Hesham <heshamoov90@gmail.com>
2021-03-15 15:56:10 +0400

    Release

--- mysql/particular_wise.php
+++ mysql/particular_wise.php
@@ -16,112 +16,96 @@ else
 
 $type = $_REQUEST['type'];
 
+$sql_header = '';
+
 if ($type == 'parent') {
     $group = ' GROUP BY familyid  order by familyid ';
     $name = 'parent';
-    $balance = 'SUM(balance) + t2.opening_balance';
-    $total = 'SUM(particular_total) ';
-    $paid = 'SUM(particular_total - balance - discount_amount)';
-    $revenue = '(SUM(particular_total) - SUM(discount_amount))';
-    $discount = 'SUM(discount_amount)';
-    $header = '<th>CHILDREN</th>';
-    $sql_header = 'COUNT(DISTINCT s.id) children';
-    $column_header = 'children';
+    $header = '<th>CHILDREN</th><th>PARENT</th>';
+    $sql_header = 'COUNT(DISTINCT s.id) children,';
+    $grade_children = 'children';
 } else {
+    $header = "<th>ADMIN NO.</th><th>STUDENT</th><th>GRADE</th>";
     $group = ' GROUP BY sid ';
     $name = 'student';
-    $header = "<th class='smallcol'>GRADE</th><th>FEE</th>";
-    $balance = '(balance) + t2.opening_balance';
-    $total = '(particular_total)';
-    $paid = '(particular_total - balance - discount_amount)';
-    $revenue = '(SUM(particular_total) - SUM(discount_amount))';
-    $discount = '(discount_amount)';
-    $header = '<th>Adm No.</th><th>Grade</th><th>Fee</th>';
-    $sql_header = 's.admission_no';
-    $column_header = 'admission_no';
+    $grade_children = 'grade';
 }
 
-
-$Fees_sql = "
-SELECT t1.familyid,parent,student,t1.sid,student,grade,contact_no,
-       $column_header,
-       $total              total,
-       $discount                discount,
-       $revenue revenue,
-       $paid paid,
-       $balance balance,
-       t2.opening_balance opening_balance,
-       ffp_id, ffp_name, master_id, master_name,
-       creation_date,amount,start_date,due_date,
-       t2.opening_balance as            opening_balance
+$student_sql = "
+SELECT 
+       student_parent_info.familyid, parent,student,admission_no,contact_no,$grade_children,
+       total,discount,revenue,paid,balance,IFNULL(opening_balance,0) opening_balance,(IFNULL(balance,0) + IFNULL(opening_balance,0)) 'net_balance'
 FROM (
-      (SELECT s.id as sid,$sql_header,s.familyid,
-              g.first_name                              'parent',      
-              CONCAT(g.mobile_phone, ' ', g.office_phone1)   'contact_no',                 
-              s.last_name AS 'student',
-              CONCAT(c.course_name, ' ', b.name)        'grade',
-              ffp.id                                    'ffp_id',
-              ffp.name                                  'ffp_name',
-              ffp.amount                                'amount',
-              ffp.created_at                            'creation_date',
-              ffc.start_date                            'start_date',
-              ffc.due_date                              'due_date',
-              SUM(ff.particular_total)                  particular_total,
-              SUM(ff.discount_amount)                   discount_amount,
-              SUM(ff.balance)                           balance, mfp.id 'master_id', mfp.name master_name
-           FROM `finance_fees` ff
-                inner join students s on ff.student_id = s.id and ff.batch_id in (select id
-                                                                                  from batches
-                                                                                  where start_date >= '$start_date'
-                                                                                    AND end_date <= '$end_date')
-                inner join guardians g on s.immediate_contact_id = g.id
-                left join batches b on s.batch_id = b.id
-                inner join courses c on b.course_id = c.id
-                inner join finance_fee_collections ffc on ff.fee_collection_id = ffc.id
-                inner join financial_years fy on ffc.financial_year_id = fy.id
-                inner join collection_particulars cp on ffc.id = cp.finance_fee_collection_id
-                INNER JOIN finance_fee_particulars ffp ON ffp.id = cp.finance_fee_particular_id and
-                                                          ((ffp.receiver_id = s.id and ffp.receiver_type = 'Student') or
-                                                           (ffp.receiver_id = s.student_category_id and
-                                                            ffp.receiver_type = 'StudentCategory' and
-                                                            ffp.batch_id = ff.batch_id) or
-                                                           (ffp.receiver_id = ff.batch_id and ffp.receiver_type = 'Batch'))
-                              INNER JOIN master_fee_particulars mfp ON ffp.master_fee_particular_id = mfp.id
-
-                LEFT JOIN finance_fee_discounts ffd ON ff.id = ffd.finance_fee_id
-       WHERE (s.is_active = 1 AND ffc.is_deleted = 0 AND b.start_date >= '$start_date' AND b.end_date <= '$end_date' $condition)
-        $group) as t1
-         
+      (SELECT s.id                                         sid,
+              s.familyid,
+              
+              g.first_name                                 'parent',$sql_header
+              
+              s.last_name 'student',
+              s.admission_no,
+              CONCAT(c.course_name, ' ', b.name)           'grade',
+              
+              CONCAT(g.mobile_phone, ' ', g.office_phone1) 'contact_no'
+       FROM students s
+                INNER JOIN guardians g on s.immediate_contact_id = g.id
+                INNER JOIN batches b on s.batch_id = b.id
+                INNER JOIN courses c on b.course_id = c.id
+       WHERE (s.is_active = 1)
+          $group
+      ) as student_parent_info
+      
+         LEFT JOIN (SELECT s.id     sid,
+                           ffc.due_date,
+                           IFNULL(SUM(ff.particular_total),'0') total,
+                           IFNULL(SUM(ffp.amount),'0') amount,
+                           IFNULL(SUM(ff.discount_amount),'0') discount,
+                           IFNULL(SUM(ff.particular_total - ff.discount_amount),'0') 'revenue',
+                           IFNULL(SUM(ff.particular_total - ff.balance - ff.discount_amount),'0') 'paid',
+                           IFNULL(SUM(ff.balance),'0') balance,
+                           mfp.id 'master_id',
+                           mfp.name master_name
+                    FROM `finance_fees` ff
+                             INNER JOIN students s on ff.student_id = s.id and ff.batch_id in
+                                   (SELECT id FROM batches WHERE start_date >= '$start_date' AND end_date <= '$end_date')
+                             INNER JOIN finance_fee_collections ffc on ff.fee_collection_id = ffc.id
+                             INNER JOIN financial_years fy on ffc.financial_year_id = fy.id
+                             INNER JOIN collection_particulars cp on ffc.id = cp.finance_fee_collection_id
+                             INNER JOIN finance_fee_particulars ffp ON ffp.id = cp.finance_fee_particular_id and
+                                                                       ((ffp.receiver_id = s.id and ffp.receiver_type = 'Student') or
+                                                                        (ffp.receiver_id = s.student_category_id and
+                                                                         ffp.receiver_type = 'StudentCategory' and
+                                                                         ffp.batch_id = ff.batch_id) or
+                                                                        (ffp.receiver_id = ff.batch_id and ffp.receiver_type = 'Batch'))
+                             INNER JOIN master_fee_particulars mfp ON ffp.master_fee_particular_id = mfp.id
+                             LEFT JOIN finance_fee_discounts ffd ON ff.id = ffd.finance_fee_id
+                    WHERE (ffc.is_deleted = 0 AND ff.balance > 0 $condition)
+                    $group) as current_fees ON student_parent_info.sid = current_fees.sid
          LEFT JOIN (
-              SELECT sum(balance) as opening_balance, s.familyid, s.id sid
-              FROM `finance_fees` ff
-              INNER JOIN students s on ff.student_id = s.id and ff.batch_id not in 
-                   (select id from batches where start_date >= '$start_date' AND end_date <= '$end_date')
-              INNER JOIN guardians g on s.immediate_contact_id = g.id
-              LEFT JOIN batches b on s.batch_id = b.id
-              INNER JOIN courses c on b.course_id = c.id
-              INNER JOIN finance_fee_collections ffc on ff.fee_collection_id = ffc.id
-              INNER JOIN financial_years fy on ffc.financial_year_id = fy.id
-              INNER JOIN collection_particulars cp on ffc.id = cp.finance_fee_collection_id
-              INNER JOIN finance_fee_particulars ffp ON ffp.id = cp.finance_fee_particular_id and
-                (
-                    (ffp.receiver_id = s.id and ffp.receiver_type = 'Student') or (ffp.receiver_id = s.student_category_id and
-                     ffp.receiver_type = 'StudentCategory' and ffp.batch_id = ff.batch_id) or
-                     (ffp.receiver_id = ff.batch_id and ffp.receiver_type = 'Batch')
-                )
-              INNER JOIN master_fee_particulars mfp ON ffp.master_fee_particular_id = mfp.id
-              LEFT JOIN finance_fee_discounts ffd ON ff.id = ffd.finance_fee_id
-              WHERE (  s.is_active = 1 AND ffc.is_deleted = 0 AND b.start_date >= '$start_date' AND b.end_date <= '$end_date' 
-              )
-                    $group
-         ) as t2 on t1.familyid = t2.familyid AND t1.sid = t2.sid
-    )
+    SELECT IFNULL(SUM(balance),'0') opening_balance, s.familyid, s.id sid
+    FROM `finance_fees` ff
+             INNER JOIN students s on ff.student_id = s.id and ff.batch_id not in
+                   (SELECT id FROM batches WHERE start_date >= '$start_date' AND end_date <= '$end_date')
+             INNER JOIN guardians g on s.immediate_contact_id = g.id
+             LEFT JOIN batches b on s.batch_id = b.id
+             INNER JOIN courses c on b.course_id = c.id
+             INNER JOIN finance_fee_collections ffc on ff.fee_collection_id = ffc.id
+             INNER JOIN financial_years fy on ffc.financial_year_id = fy.id
+             INNER JOIN collection_particulars cp on ffc.id = cp.finance_fee_collection_id
+             INNER JOIN finance_fee_particulars ffp ON ffp.id = cp.finance_fee_particular_id and
+                                                       ((ffp.receiver_id = s.id and ffp.receiver_type = 'Student') or
+                                                        (ffp.receiver_id = s.student_category_id and
+                                                         ffp.receiver_type = 'StudentCategory' and
+                                                         ffp.batch_id = ff.batch_id) or
+                                                        (ffp.receiver_id = ff.batch_id and ffp.receiver_type = 'Batch'))
+             INNER JOIN master_fee_particulars mfp ON ffp.master_fee_particular_id = mfp.id
+             LEFT JOIN finance_fee_discounts ffd ON ff.id = ffd.finance_fee_id
+    WHERE (s.is_active = 1 AND ffc.is_deleted = 0)
     $group
-    ";
-
-
-//echo $Fees_sql;
-$result = $conn->query($Fees_sql);
+) as prev_balance ON student_parent_info.sid = prev_balance.sid
+    )
+";
+//echo $student_sql;
+$result = $conn->query($student_sql);
 $rowNumber = 1;
 if ($result->num_rows > 0) {
     echo "<table class='table table-bordered table-striped table-hover' id='DefaultersTable'>";
@@ -130,7 +114,6 @@ if ($result->num_rows > 0) {
         <tr>
     		<th>#</th>
     		<th width='20'>FAMILY ID</th>
-            <th>NAME</th>
             <th width='20'>MOB. #</th>
             $header
     		<th>TOTAL</th>
@@ -139,38 +122,32 @@ if ($result->num_rows > 0) {
     		<th>PAID</th>
     		<th>BALANCE</th>    		
     		<th>OPENING <BR> BALANCE</th>
-    		<th>TOTAL BALANCE</th>
+    		<th>NET BALANCE</th>
     	</tr>
         </thead>
         <tbody>
     ";
     while ($row = $result->fetch_assoc()) {
-        if ($row['balance'] > 0) {
-            echo "
-    	<tr>
-    		<td>" . $rowNumber . "</td>
-    		<td  class='textLeft'>" . $row['familyid'] . "</td>
-            <td  class='textLeft'>" . $row[$name] . "</td>
-            <td>" . $row['contact_no'] . "</td>
-            <td>" . $row[$column_header] . "</td>";
+        echo "
+    	        <tr>
+    		        <td>" . $rowNumber . "</td>
+    		        <td  class='textLeft'>" . $row['familyid'] . "</td>
+                    <td>" . $row['contact_no'] . "</td>";
 
-            if ($type == 'student') {
-                echo "<td class='textLeft'>" . $row['grade'] . "</td>";
-                echo "<td class='textLeft'>" . $row['master_name'] . "</td>";
-            }
+        if ($type == 'student')
+            echo "<td>" . $row['admission_no'] . "</td><td>" . $row['student'] . "</td><td>" . $row['grade'] . "</td>";
+        else
+            echo "<td>" . $row['parent'] . "</td><td>" . $row['children'] . "</td>";
 
-
-            echo "
-        <td class='textRight'>" . number_format((float)$row['total'], 2) . "</td>
-        <td class='textRight'>" . number_format((float)$row['discount'], 2) . "</td>
-        <td class='textRight'>" . number_format((float)$row['revenue'], 2) . "</td>
-        <td class='textRight'>" . number_format((float)$row['paid'], 2) . "</td>
-        <td class='textRight'>" . number_format((float)$row['balance']-$row['opening_balance'], 2) . "</td>
-        <td class='textRight'>" . number_format((float)$row['opening_balance'], 2) . "</td>
-        <td class='textRight'>" . number_format((float)$row['balance'], 2) . "</td>";
-
-            $rowNumber++;
-        }
+        echo "
+                    <td class='textRight'>" . number_format((float)$row['total'], 2) . "</td>
+                    <td class='textRight'>" . number_format((float)$row['discount'], 2) . "</td>
+                    <td class='textRight'>" . number_format((float)$row['revenue'], 2) . "</td>
+                    <td class='textRight'>" . number_format((float)$row['paid'], 2) . "</td>
+                    <td class='textRight'>" . number_format((float)$row['balance'], 2) . "</td>
+                    <td class='textRight'>" . number_format((float)$row['opening_balance'], 2) . "</td>
+                    <td class='textRight'>" . number_format((float)$row['net_balance'], 2) . "</td>";
+        $rowNumber++;
     }
     echo '</tbody></table>';
 } else {
