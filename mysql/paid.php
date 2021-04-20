47bfff150ce5f3940985d2c16909c07593f4ddbd
Jamsheed Khalid <jamsheedkhalid35@gmail.com>
2021-04-20 09:59:03 +0400

    'fix-code'

--- mysql/paid.php
+++ mysql/paid.php
@@ -54,12 +54,20 @@ FROM (
               s.admission_no,
               CONCAT(c.course_name, ' ', b.name)           'grade',
               g.mobile_phone 'contact_no'
-       FROM students s
-                INNER JOIN guardians g on s.immediate_contact_id = g.id
+       FROM(select id, familyid, last_name, admission_no, batch_id, immediate_contact_id
+             from students
+             union all
+             select former_id, familyid, last_name, admission_no, batch_id, immediate_contact_id
+             from archived_students) s
+                inner JOIN
+            (select id, first_name, mobile_phone
+             from guardians
+             union
+             select former_id, first_name, mobile_phone
+             from archived_guardians) g on s.immediate_contact_id = g.id
                 INNER JOIN batches b on s.batch_id = b.id
-                INNER JOIN courses c on b.course_id = c.id
-       WHERE (s.is_active = 1)
-          $group
+                INNER JOIN courses c on b.course_id = c.id    
+           GROUP BY s.id
       ) as student_parent_info
          INNER JOIN (SELECT s.id     sid,s.familyid,ffc.name fee_name,
                            ffc.due_date,
@@ -67,12 +75,18 @@ FROM (
                            IFNULL(SUM(ffp.amount),'0') amount,
                            IFNULL(SUM(ff.discount_amount),'0') discount,
                            IFNULL(SUM(ff.particular_total - ff.discount_amount),'0') 'revenue',
-                           IFNULL(SUM(ff.particular_total - ff.balance - ff.discount_amount),'0') 'paid',
+                           IFNULL(SUM(ft.amount),'0') 'paid',
                            IFNULL(SUM(ff.balance),'0') balance,
                            mfp.id 'master_id',
                            mfp.name master_name
-                    FROM `finance_fees` ff
-                             INNER JOIN students s on ff.student_id = s.id 
+                    FROM finance_transactions ft
+             inner join(
+        select id, student_category_id, familyid, immediate_contact_id
+        from students
+        union
+        select former_id, student_category_id, familyid, immediate_contact_id
+        from archived_students) s on ft.payee_id = s.id
+             INNER JOIN `finance_fees` ff on ft.finance_id = ff.id
                              INNER JOIN finance_fee_collections ffc on ff.fee_collection_id = ffc.id
                              INNER JOIN financial_years fy on ffc.financial_year_id = fy.id
                              INNER JOIN collection_particulars cp on ffc.id = cp.finance_fee_collection_id
@@ -82,15 +96,13 @@ FROM (
                                      ffp.receiver_type = 'StudentCategory' and
                                      ffp.batch_id = ff.batch_id) or
                                     (ffp.receiver_id = ff.batch_id and ffp.receiver_type = 'Batch'))
-                             INNER JOIN master_fee_particulars mfp ON ffp.master_fee_particular_id = mfp.id
-                        inner join finance_transactions ft
-                                         on ff.id = ft.finance_id and (ft.payee_id = s.id and ft.payee_type = 'Student')                        
+                             INNER JOIN master_fee_particulars mfp ON ffp.master_fee_particular_id = mfp.id                  
                              LEFT JOIN finance_fee_discounts ffd ON ff.id = ffd.finance_fee_id 
                     WHERE (ffc.is_deleted = 0 AND ff.balance != ff.particular_total $condition 
-                    AND (ff.particular_total - ff.balance - ff.discount_amount > 0)
-                        AND 
-                            ft.transaction_date between '$start_date' and '$end_date')
-                    $group) as current_fees ON student_parent_info.familyid = current_fees.familyid and student_parent_info.sid = current_fees.sid
+                    AND (ff.particular_total - ff.balance - ff.discount_amount > 0)                       
+                         and ft.payee_type = 'Student' and
+                           ( ft.transaction_date between '$start_date' and '$end_date'))
+                      $group) as current_fees ON student_parent_info.familyid = current_fees.familyid and student_parent_info.sid = current_fees.sid
                     
     ) $order
 ";
@@ -98,6 +110,7 @@ FROM (
 $result = $conn->query($student_sql);
 $rowNumber = 1;
 if ($result->num_rows > 0) {
+    $total = 0.0;
 //    echo '<h4>No. ' . mysqli_num_rows($result) . '</h4>';
     echo "<table class='table table-bordered table-striped table-hover' id='PaidTable'>";
     echo "
@@ -135,8 +148,11 @@ if ($result->num_rows > 0) {
                     <td class='textRight'>" . number_format((float)$row['paid'], 2) . "</td>
                     <td class='textRight'>" . number_format((float)$row['balance'], 2) . "</td>";
         $rowNumber++;
+        $total = $total + $row['paid'];
     }
     echo '</tbody></table>';
+
+    echo "<h2> TOTAL: AED " . $total . "</h2>";
 } else {
     echo 'No Data Found! try another search . ';
 }
